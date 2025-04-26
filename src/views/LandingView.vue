b7aa53217cee55036cdd2d90cca847861dbca4b3
Ari <acas_2014@outlook.es>
2025-04-26 01:00:08 -0500

    feat(navbar): add Navbar component

--- src/views/LandingView.vue
+++ src/views/LandingView.vue
@@ -1,3 +1,32 @@
 <template>
-  <h1>Setup</h1>
+  <div class="landing-view">
+    <NavbarComponent />
+  </div>
 </template>
+
+<script setup lang="ts">
+import NavbarComponent from '@/components/common/NavbarComponent.vue'
+</script>
+
+<style scoped>
+.landing-view {
+  background: var(--color-background);
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: start;
+  min-height: 100vh;
+  width: 100vw;
+  padding: 0 8%;
+  scroll-behavior: smooth;
+}
+
+#home,
+#producto,
+#beneficios,
+#planes,
+#faq {
+  width: 100%;
+  scroll-margin-top: 100px;
+}
+</style>
